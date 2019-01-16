88ecc72909c4db68b08ac4428e393a143a1004d2
석훈 이 <nolru7@naver.com>
2019-01-16 13:50:44 +0900

    디비 테스트

---
+++ Solution/ClassLibrary/DataBase.cs
@@ -0,0 +1,45 @@
+﻿using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+
+namespace ClassLibrary
+{
+    public class DataBase
+    {
+        public MySqlConnection GetConnection()
+        {
+            try
+            {
+                MySqlConnection conn = new MySqlConnection();
+
+                string path = "\\DBInfo.json";
+                string result = new StreamReader(File.OpenRead(path)).ReadToEnd();
+                JObject jo = JsonConvert.DeserializeObject<JObject>(result);
+                Hashtable map = new Hashtable();
+                foreach (JProperty col in jo.Properties())
+                {
+                    Console.WriteLine("{0} : {1}", col.Name, col.Value);
+                    map.Add(col.Name, col.Value);
+                }
+                #region 
+                string strConnection1 =
+                    string.Format("server={0};user={1};password={2};database={3};", map["server"], map["user"], map["password"], map["database"]);
+                conn.ConnectionString = strConnection1;
+                conn.Open();
+                #endregion
+                return conn;
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+    }
+}
